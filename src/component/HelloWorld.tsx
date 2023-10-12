8738adcf13e7a03a189ba48e0eb3210dc1708798
yejinzhan <jim.ye1@miniso.com>
2023-10-12 18:54:24 +0800

    add log

--- src/component/HelloWorld.tsx
+++ src/component/HelloWorld.tsx
@@ -2,8 +2,13 @@ import { useEffect } from 'react';
 
 export default function Welcome(props) {
   useEffect(() => {
+    console.log('deps useEffect')
     document.title = `Hello, ${props.name}`
   }, [props.name])
+
+  useEffect(() => console.log('mounted'), []);
+
+  useEffect(() => console.log('mounted or updated'));
 
   return <h1>Hello, {props.name}</h1>
 }
